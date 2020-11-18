0f1d70a7af31e4420b74626b3bd14d081e2f9e13
Callie Stump <callie@stu.mp>
2020-11-18 16:20:54 -0500

    intializes env

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
-
+const Dotenv = require("dotenv-webpack");
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -18,7 +18,8 @@ module.exports = {
       title: 'title',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
